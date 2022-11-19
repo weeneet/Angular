2728fb9d47ff1e8697f14d1c6a0f8ae30123fa71
weeneet <vinittamboli31@gmail.com>
2022-11-19 13:10:19 +0530

    first commit

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,19 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+
+  constructor() { }
+  Recipes: Recipe[] = [
+    new Recipe('Paneer Masala', 'Paneer Masala with gravy and lots of Butter', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&resize=768,574')
+  ];
+
+  ngOnInit(): void {
+  }
+
+}
